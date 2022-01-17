e72b8dcb0e4520ae8665483f3ce1632b38f3c0d7
yosim <JosefTheKing17>
2022-01-18 01:27:18 +0200

    Something went worng

--- src/ex4_java_client/Main.java
+++ src/ex4_java_client/Main.java
@@ -150,7 +150,6 @@ public class Main implements Runnable {
         }
         if (lst == null) {
             lst = new ArrayList<>(gameManger.getGraphAlgo().shortestPath(agent.getSrc(), centerNode));
-            System.out.println("Is HERE?");
         }
         return lst.get(1).getKey();
 
